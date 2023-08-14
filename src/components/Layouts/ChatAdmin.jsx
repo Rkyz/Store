ba1d40050775bec9eca7aa14729e72d52a966b53
Rkyz <rioalamsyah@smkwikrama.sch.id>
2023-08-14 19:33:37 +0700

    Memperbaiki Chat Admin Responsive

--- src/components/Layouts/ChatAdmin.jsx
+++ src/components/Layouts/ChatAdmin.jsx
@@ -3,11 +3,11 @@ import {MdOutlineClose} from 'react-icons/md'
 const ChatAdmin = ({openCs, toggleOpenCs}) => {
     return (
         <div
-            className={`text-white bg-blue-500 z-50  bottom-0 fixed h-[550px] w-[400px] right-[30px] rounded-3xl ${openCs
+            className={`text-white bg-blue-500 z-50  bottom-0 fixed lg:h-[550px] lg:w-[400px] right-[30px] rounded-3xl ${openCs
                 ? ' bottom-[30px]'
-                : 'h-0 w-0'} `}>
+                : 'lg:h-0 lg:w-0'} `}>
             <div
-                className={`w-full bg-yellow-500 h-[50px] rounded-t-2xl flex justify-between items-center px-[15px] ${openCs ? '':'hidden'}`}>
+                className={`lg:w-full bg-yellow-500 h-[50px] rounded-t-2xl flex justify-between items-center max-lg:hidden px-[15px] ${openCs ? '':' lg:hidden'}`}>
                 <ul className="flex">
                     <li className="flex items-center mr-1">
                         <div className="w-[5px] h-[5px] bg-black rounded-full"></div>
